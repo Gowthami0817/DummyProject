757a39b40351029221617c47ba886cac65574cf4
91830 <91830@LAPTOP-JPHJ2F9N>
2025-04-24 14:15:24 +0530

    changed implementation

--- DummyProject/src/test/java/Package1/ReadTest.java
+++ DummyProject/src/test/java/Package1/ReadTest.java
@@ -5,7 +5,7 @@ import org.testng.annotations.Test;
 public class ReadTest {
 	@Test
 	public void WriteTest() {
-		System.out.println("Write test executed successfully");
+		System.out.println("Writing test executed successfully");
 	}
 
 
