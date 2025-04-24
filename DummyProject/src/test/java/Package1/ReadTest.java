d4be258b6af0c2ecfe659c144a19981deb1ac09e
91830 <91830@LAPTOP-JPHJ2F9N>
2025-04-24 18:49:17 +0530

    chnages done

--- DummyProject/src/test/java/Package1/ReadTest.java
+++ DummyProject/src/test/java/Package1/ReadTest.java
@@ -5,7 +5,7 @@ import org.testng.annotations.Test;
 public class ReadTest {
 	@Test
 	public void WriteTest() {
-		System.out.println("Writing test executed successfully");
+		System.out.println("Writed test executed successfully");
 	}
 
 
